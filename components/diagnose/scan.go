4559fcff01d253821e5589805167e8dc916f7930
Gyuho Lee <gyuhox@gmail.com>
2024-08-28 00:20:43 +0800

    scan
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- components/diagnose/scan.go
+++ components/diagnose/scan.go
@@ -86,27 +86,40 @@ func Scan(ctx context.Context, opts ...OpOption) error {
 				if op.pollGPMEvents {
 					fmt.Printf("\n%s checking nvidia GPM events\n", inProgress)
 
-					select {
-					case <-ctx.Done():
-						log.Logger.Warnw("context done")
-
-					case <-time.After(time.Minute + 10*time.Second):
-						fmt.Printf("%s no gpm events found after 70 seconds\n", checkMark)
-
-					case event := <-nvidia_query_nvml.DefaultInstance().RecvGPMEvents():
-						if event != nil && event.Error != nil {
-							fmt.Printf("%s received the gpm event with an error %v\n", checkMark, event.Error)
+					gpmSupported, err := nvidia_query_nvml.GPMSupported()
+					if err == nil {
+						if gpmSupported {
+							log.Logger.Infow("auto-detected gpm supported")
 						} else {
-							if nvidia_query_nvml.DefaultInstance().GPMMetricsSupported() {
-								fmt.Printf("%s successfully received the gpm event with no error\n", checkMark)
-							} else {
-								fmt.Printf("%s gpm metrics not supported\n", checkMark)
-							}
+							log.Logger.Infow("auto-detected gpm not supported -- skipping", "error", err)
 						}
+					} else {
+						log.Logger.Warnw("failed to check gpm supported or not", "error", err)
+					}
 
-						yb, _ := event.YAML()
-						fmt.Println(string(yb))
-						println()
+					if gpmSupported {
+						select {
+						case <-ctx.Done():
+							log.Logger.Warnw("context done")
+
+						case <-time.After(time.Minute + 10*time.Second):
+							fmt.Printf("%s no gpm events found after 70 seconds\n", checkMark)
+
+						case event := <-nvidia_query_nvml.DefaultInstance().RecvGPMEvents():
+							if event != nil && event.Error != nil {
+								fmt.Printf("%s received the gpm event with an error %v\n", checkMark, event.Error)
+							} else {
+								if nvidia_query_nvml.DefaultInstance().GPMMetricsSupported() {
+									fmt.Printf("%s successfully received the gpm event with no error\n", checkMark)
+								} else {
+									fmt.Printf("%s gpm metrics not supported\n", checkMark)
+								}
+							}
+
+							yb, _ := event.YAML()
+							fmt.Println(string(yb))
+							println()
+						}
 					}
 				}
 			}
