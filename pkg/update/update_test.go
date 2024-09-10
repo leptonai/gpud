081c368ccf7d69d65b07c186225e547fe34e87b4
Hitoshi Mitake <hitoshi@lepton.ai>
2024-09-10 17:40:33 +0900

    test fix

--- pkg/update/update_test.go
+++ pkg/update/update_test.go
@@ -1,12 +1,18 @@
 package update
 
 import (
+	"fmt"
 	"testing"
 )
 
 func Test_tarballName(t *testing.T) {
+	ubuntuVer := detectUbuntuVersion()
+	ubuntuSuffix := ""
+	if ubuntuVer != "" {
+		ubuntuSuffix = "_" + ubuntuVer
+	}
 	name := tarballName("v0.0.1", "linux", "amd64")
-	want := "gpud_v0.0.1_linux_amd64.tgz"
+	want := fmt.Sprintf("gpud_v0.0.1_linux_amd64%s.tgz", ubuntuSuffix)
 	if name != want {
 		t.Fatalf("want: %s, got: %s", want, name)
 	}
