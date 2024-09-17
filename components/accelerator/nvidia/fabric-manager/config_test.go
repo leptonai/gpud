90bbcfd68540bc6b7113a1a320a2b051ebed3fcf
Gyuho Lee <gyuhox@gmail.com>
2024-09-17 17:36:16 +0800

    fix lint
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- components/accelerator/nvidia/fabric-manager/config_test.go
+++ components/accelerator/nvidia/fabric-manager/config_test.go
@@ -15,11 +15,12 @@ func TestRegexNVSwitchFatalSXidFromLog(t *testing.T) {
 		{"[Jul 23 2024 07:53:55] [ERROR] [tid 841] detected NVSwitch fatal error 20034 on fid 0 on NVSwitch pci bus id 00000000:86:00.0 physical id 3 port 33", true},
 	}
 
+	re, err := regexp.Compile(regexNVSwitchFatalSXidFromLog)
+	if err != nil {
+		t.Fatalf("Error compiling regex: %v", err)
+	}
 	for _, test := range tests {
-		matched, err := regexp.MatchString(regexNVSwitchFatalSXidFromLog, test.log)
-		if err != nil {
-			t.Fatalf("Error matching regex: %v", err)
-		}
+		matched := re.MatchString(test.log)
 		if matched != test.matches {
 			t.Errorf("Expected match: %v, got: %v for log: %s", test.matches, matched, test.log)
 		}
@@ -36,11 +37,12 @@ func TestRegexNVSwitchNonFatalSXidFromLog(t *testing.T) {
 		{"[Jul 09 2024 18:14:07] [ERROR] [tid 12727] detected NVSwitch non-fatal error 12028 on fid 0 on NVSwitch pci bus id 00000000:86:00.0 physical id 3 port 61", true},
 	}
 
+	re, err := regexp.Compile(regexNVSwitchNonFatalSXidFromLog)
+	if err != nil {
+		t.Fatalf("Error compiling regex: %v", err)
+	}
 	for _, test := range tests {
-		matched, err := regexp.MatchString(regexNVSwitchNonFatalSXidFromLog, test.log)
-		if err != nil {
-			t.Fatalf("Error matching regex: %v", err)
-		}
+		matched := re.MatchString(test.log)
 		if matched != test.matches {
 			t.Errorf("Expected match: %v, got: %v for log: %s", test.matches, matched, test.log)
 		}
@@ -57,11 +59,12 @@ func TestRegexNVSwitchNVLinkFailureFromLog(t *testing.T) {
 		{"[Sep 17 2024 06:01:46] [ERROR] [tid 1230079] failed to find the GPU handle 5410063385821516767 in the multicast team request setup 6130285411925746235.", true},
 	}
 
+	re, err := regexp.Compile(regexNVSwitchNVLinkFailureFromLog)
+	if err != nil {
+		t.Fatalf("Error compiling regex: %v", err)
+	}
 	for _, test := range tests {
-		matched, err := regexp.MatchString(regexNVSwitchNVLinkFailureFromLog, test.log)
-		if err != nil {
-			t.Fatalf("Error matching regex: %v", err)
-		}
+		matched := re.MatchString(test.log)
 		if matched != test.matches {
 			t.Errorf("Expected match: %v, got: %v for log: %s", test.matches, matched, test.log)
 		}
