8b44d494ddce36345939bec9000275987838aaff
Gyuho Lee <gyuhox@gmail.com>
2024-09-25 22:32:27 +0800

    improve error message
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- components/accelerator/nvidia/remapped-rows/component_output.go
+++ components/accelerator/nvidia/remapped-rows/component_output.go
@@ -112,7 +112,7 @@ func (o *Output) Evaluate() (string, bool, error) {
 		}
 		if rma {
 			healthy = false
-			reasons = append(reasons, fmt.Sprintf("nvidia-smi GPU %s qualifies for RMA (failure occurred %v, uncorrectable errors %s)", r.ID, r.RemappingFailed, r.RemappedDueToUncorrectableErrors))
+			reasons = append(reasons, fmt.Sprintf("nvidia-smi GPU %s qualifies for RMA (remapping failure occurred %v, remapped due to uncorrectable errors %s)", r.ID, r.RemappingFailed, r.RemappedDueToUncorrectableErrors))
 		}
 
 		needsReset, err := r.RequiresReset()
@@ -130,7 +130,7 @@ func (o *Output) Evaluate() (string, bool, error) {
 	for _, r := range o.RemappedRowsNVML {
 		if r.QualifiesForRMA() {
 			healthy = false
-			reasons = append(reasons, fmt.Sprintf("nvml GPU %s qualifies for RMA (failure occurred %v, uncorrectable errors %d)", r.UUID, r.RemappingFailed, r.RemappedDueToUncorrectableErrors))
+			reasons = append(reasons, fmt.Sprintf("nvml GPU %s qualifies for RMA (remapping failure occurred %v, remapped due to uncorrectable errors %d)", r.UUID, r.RemappingFailed, r.RemappedDueToUncorrectableErrors))
 		}
 		if r.RequiresReset() {
 			healthy = false
