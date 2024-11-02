8d234d8e5b96f7bd33e41ffa1927999b3df7cc79
Gyuho Lee <gyuhox@gmail.com>
2024-11-02 17:53:06 +0800

    fix(gpud): add explicit version sub-command, flag for early exit
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- cmd/gpud/main.go
+++ cmd/gpud/main.go
@@ -5,9 +5,16 @@ import (
 	"os"
 
 	"github.com/leptonai/gpud/cmd/gpud/command"
+	"github.com/leptonai/gpud/version"
 )
 
 func main() {
+	// check for version flag before initializing the full app
+	if len(os.Args) == 2 && (os.Args[1] == "version" || os.Args[1] == "--version" || os.Args[1] == "-v") {
+		fmt.Println("gpud version", version.Version)
+		return
+	}
+
 	app := command.App()
 	if err := app.Run(os.Args); err != nil {
 		fmt.Fprintf(os.Stderr, "gpud: %s\n", err)
