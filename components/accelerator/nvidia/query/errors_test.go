91699bd7cf8e08d497a7aba61556a730f54cf24b
Gyuho Lee <gyuhox@gmail.com>
2024-11-22 12:42:28 +0800

    more test
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- components/accelerator/nvidia/query/errors_test.go
+++ components/accelerator/nvidia/query/errors_test.go
@@ -36,6 +36,26 @@ func TestIsErrDeviceHandleUnknownError(t *testing.T) {
 			err:  errors.New("error getting device handle but not"),
 			want: false,
 		},
+		{
+			name: "unable to determine device handle error",
+			err:  errors.New("Unable to determine the device handle for GPU0000:CB:00.0: Unknown Error"),
+			want: true,
+		},
+		{
+			name: "unable to determine device handle error different case",
+			err:  errors.New("UNABLE TO DETERMINE THE DEVICE HANDLE FOR GPU0000:CB:00.0: UNKNOWN ERROR"),
+			want: true,
+		},
+		{
+			name: "contains unknown error but not device handle related",
+			err:  errors.New("some unknown error occurred"),
+			want: false,
+		},
+		{
+			name: "partial match for unable to determine",
+			err:  errors.New("unable to determine something else"),
+			want: false,
+		},
 	}
 
 	for _, tt := range tests {
