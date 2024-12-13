d95efb5c658d9171741f6d96c15ba0cdb7a40c4b
Gyuho Lee <gyuhox@gmail.com>
2024-12-13 22:03:59 +0800

    skip doesn't exist mount point
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- pkg/disk/disk.go
+++ pkg/disk/disk.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"sort"
 	"strconv"
+	"strings"
 
 	"github.com/leptonai/gpud/log"
 
@@ -53,6 +54,13 @@ func GetPartitions(ctx context.Context, opts ...OpOption) (Partitions, error) {
 		if part.Mounted {
 			part.Usage, err = GetUsage(ctx, p.Mountpoint)
 			if err != nil {
+				// mount point is gone
+				// e.g., "no such file or directory"
+				if strings.Contains(err.Error(), "no such file or directory") {
+					log.Logger.Debugw("skipping partition because mount point does not exist", "error", err, "device", part.Device, "mountPoint", part.MountPoint)
+					continue
+				}
+
 				return nil, err
 			}
 		}
