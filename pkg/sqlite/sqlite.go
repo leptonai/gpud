b4a0343cfccb804e3dc9f737294618dcea1b382f
Gyuho Lee <gyuhox@gmail.com>
2025-01-02 17:07:56 +0800

    feat(sqlite): create a separate read-only mode instance
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- pkg/sqlite/sqlite.go
+++ pkg/sqlite/sqlite.go
@@ -8,12 +8,33 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func Open(file string) (*sql.DB, error) {
-	// no need to run separate PRAGMA commands
+// Helper function to open a SQLite3 database.
+// ref. https://github.com/mattn/go-sqlite3/blob/7658c06970ecf5588d8cd930ed1f2de7223f1010/sqlite3.go#L975
+func Open(file string, opts ...OpOption) (*sql.DB, error) {
+	op := &Op{}
+	if err := op.applyOpts(opts); err != nil {
+		return nil, err
+	}
+
+	// Build connection string in URI format
+	// ref. https://www.sqlite.org/c3ref/open.html
+	// ref. https://www.sqlite.org/uri.html
+	// ref. https://github.com/mattn/go-sqlite3?tab=readme-ov-file#connection-string
+	conns := "file:" + file
+
+	// Add URI parameters
 	// ref. https://www.sqlite.org/pragma.html#pragma_busy_timeout
 	// ref. https://www.sqlite.org/pragma.html#pragma_journal_mode
 	// ref. https://www.sqlite.org/pragma.html#pragma_synchronous
-	conns := fmt.Sprintf("%s?_busy_timeout=5000&_journal_mode=WAL&_synchronous=NORMAL", file)
+	conns += "?_busy_timeout=5000&_journal_mode=WAL&_synchronous=NORMAL&"
+
+	if op.readOnly {
+		conns += "&mode=ro"
+	}
+
+	fmt.Println(conns)
+
+	// Open with URI format enabled
 	db, err := sql.Open("sqlite3", conns)
 	if err != nil {
 		return nil, err
