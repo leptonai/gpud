5e4a7e15e744af1e1e1df24d915f3347ec8a25c4
Gyuho Lee <gyuhox@gmail.com>
2025-01-09 14:26:52 +0800

    fix(nvml): set "supported" boolean on clock speed "not supported" error return
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- components/accelerator/nvidia/query/nvml/clock_speed.go
+++ components/accelerator/nvidia/query/nvml/clock_speed.go
@@ -16,15 +16,23 @@ type ClockSpeed struct {
 
 	GraphicsMHz uint32 `json:"graphics_mhz"`
 	MemoryMHz   uint32 `json:"memory_mhz"`
+
+	// Supported is true if the clock speed is supported by the device.
+	Supported bool `json:"supported"`
 }
 
 func GetClockSpeed(uuid string, dev device.Device) (ClockSpeed, error) {
 	clockSpeed := ClockSpeed{
-		UUID: uuid,
+		UUID:      uuid,
+		Supported: true,
 	}
 
 	// ref. https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g2efc4dd4096173f01d80b2a8bbfd97ad
 	graphicsClock, ret := dev.GetClockInfo(nvml.CLOCK_GRAPHICS)
+	if IsNotSupportError(ret) {
+		clockSpeed.Supported = false
+		return clockSpeed, nil
+	}
 	if ret != nvml.SUCCESS {
 		return ClockSpeed{}, fmt.Errorf("failed to get device clock info for nvml.CLOCK_GRAPHICS: %v", nvml.ErrorString(ret))
 	}
@@ -32,6 +40,10 @@ func GetClockSpeed(uuid string, dev device.Device) (ClockSpeed, error) {
 
 	// ref. https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g2efc4dd4096173f01d80b2a8bbfd97ad
 	memClock, ret := dev.GetClockInfo(nvml.CLOCK_MEM)
+	if IsNotSupportError(ret) {
+		clockSpeed.Supported = false
+		return clockSpeed, nil
+	}
 	if ret != nvml.SUCCESS {
 		return ClockSpeed{}, fmt.Errorf("failed to get device clock info for nvml.CLOCK_MEM: %v", nvml.ErrorString(ret))
 	}
