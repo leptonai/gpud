17062000bf9019d6f6083a7f393fce1a702f807a
Gyuho Lee <gyuhox@gmail.com>
2025-01-16 20:24:10 +0800

    fix(nvidia): increase hw slowdown event level to critical (from info)
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- components/accelerator/nvidia/hw-slowdown/component.go
+++ components/accelerator/nvidia/hw-slowdown/component.go
@@ -82,7 +82,7 @@ func (c *component) Events(ctx context.Context, since time.Time) ([]components.E
 		convertedEvents = append(convertedEvents, components.Event{
 			Time:    metav1.Time{Time: time.Unix(event.UnixSeconds, 0).UTC()},
 			Name:    EventNameHWSlowdown,
-			Type:    components.EventTypeInfo,
+			Type:    components.EventTypeCritical,
 			Message: strings.Join(event.Reasons, ", "),
 			ExtraInfo: map[string]string{
 				EventKeyGPUUUID: event.GPUUUID,
