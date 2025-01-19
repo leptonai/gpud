59528ef2be2bd49d88e583833179085a628267c8
Gyuho Lee <gyuhox@gmail.com>
2025-01-19 14:26:31 +0800

    feat(pkg/dmesg): common dmesg log poller helper functions
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- pkg/dmesg/dmesg.go
+++ pkg/dmesg/dmesg.go
@@ -4,7 +4,6 @@ package dmesg
 import (
 	"bytes"
 	"errors"
-	"regexp"
 	"time"
 )
 
@@ -18,10 +17,10 @@ var (
 	shortIsoTimeFormatN = len(shortIsoTimeFormat)
 )
 
-// ParseISOtimeWithError parses the timestamp from "dmesg --time-format=iso" output lines.
+// ParseDmesgTimeISO parses the timestamp from "dmesg --time-format=iso" output lines.
 // ref.
 // "The definition of the iso timestamp is: YYYY-MM-DD<T>HH:MM:SS,<microseconds>←+><timezone offset from UTC>."
-func ParseISOtimeWithError(line []byte) (time.Time, []byte, error) {
+func ParseDmesgTimeISO(line []byte) (time.Time, []byte, error) {
 	if len(line) < isoTimeFormatN {
 		return time.Time{}, nil, errors.New("line is too short")
 	}
@@ -36,8 +35,8 @@ func ParseISOtimeWithError(line []byte) (time.Time, []byte, error) {
 	return parsedTime, extractedLine, nil
 }
 
-// ParseShortISOtimeWithError parses the timestamp from "journalctl -o short-iso" output lines.
-func ParseShortISOtimeWithError(line []byte) (time.Time, []byte, error) {
+// ParseJournalctlTimeShortISO parses the timestamp from "journalctl -o short-iso" output lines.
+func ParseJournalctlTimeShortISO(line []byte) (time.Time, []byte, error) {
 	if len(line) < shortIsoTimeFormatN {
 		return time.Time{}, nil, errors.New("line is too short")
 	}
@@ -50,23 +49,4 @@ func ParseShortISOtimeWithError(line []byte) (time.Time, []byte, error) {
 
 	extractedLine := bytes.TrimSpace(line[shortIsoTimeFormatN:])
 	return parsedTime, extractedLine, nil
-}
-
-var regexForDmesgTime = regexp.MustCompile(`^\[([^\]]+)\]`)
-
-// ParseCtimeWithError Parses the timestamp from "dmesg --ctime" output lines.
-// Returns a zero time if the timestamp is not found or the timestamp is invalid.
-// Returns an error if the timestamp is not found or the timestamp is invalid.
-func ParseCtimeWithError(line []byte) (time.Time, []byte, error) {
-	matches := regexForDmesgTime.FindStringSubmatch(string(line))
-	if len(matches) == 0 {
-		return time.Time{}, nil, errors.New("no timestamp matches found")
-	}
-
-	s := matches[1]
-	timestamp, err := time.Parse(time.ANSIC, s)
-	if err != nil {
-		return time.Time{}, nil, err
-	}
-	return timestamp, line, nil
 }
