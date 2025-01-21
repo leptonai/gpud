13439a25292115d501f8ca900474f33c043400b1
Gyuho Lee <gyuhox@gmail.com>
2025-01-21 23:21:01 +0800

    fix(tailscale): remove unused version checks
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- components/tailscale/component.go
+++ components/tailscale/component.go
@@ -3,26 +3,18 @@ package tailscale
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/leptonai/gpud/components"
-	"github.com/leptonai/gpud/components/query"
 	tailscale_id "github.com/leptonai/gpud/components/tailscale/id"
 	"github.com/leptonai/gpud/log"
 )
 
 func New(ctx context.Context, cfg Config) components.Component {
 	cfg.Query.SetDefaultsIfNotSet()
-	setDefaultPoller(cfg)
-
-	cctx, ccancel := context.WithCancel(ctx)
-	getDefaultPoller().Start(cctx, cfg.Query, tailscale_id.Name)
 
 	return &component{
 		rootCtx: ctx,
-		cancel:  ccancel,
-		poller:  getDefaultPoller(),
 	}
 }
 
@@ -30,52 +22,17 @@ var _ components.Component = (*component)(nil)
 
 type component struct {
 	rootCtx context.Context
-	cancel  context.CancelFunc
-	poller  query.Poller
 }
 
 func (c *component) Name() string { return tailscale_id.Name }
 
 func (c *component) States(ctx context.Context) ([]components.State, error) {
-	last, err := c.poller.Last()
-	if err == query.ErrNoData { // no data
-		log.Logger.Debugw("nothing found in last state (no data collected yet)", "component", tailscale_id.Name)
-		return []components.State{
-			{
-				Name:    tailscale_id.Name,
-				Healthy: true,
-				Reason:  query.ErrNoData.Error(),
-			},
-		}, nil
-	}
-	if err != nil {
-		return nil, err
-	}
-	if last.Error != nil {
-		return []components.State{
-			{
-				Name:    tailscale_id.Name,
-				Healthy: false,
-				Error:   last.Error.Error(),
-				Reason:  "last query failed",
-			},
-		}, nil
-	}
-	if last.Output == nil {
-		return []components.State{
-			{
-				Name:    tailscale_id.Name,
-				Healthy: true,
-				Reason:  "no output",
-			},
-		}, nil
-	}
-
-	output, ok := last.Output.(*Output)
-	if !ok {
-		return nil, fmt.Errorf("invalid output type: %T", last.Output)
-	}
-	return output.States()
+	return []components.State{
+		{
+			Name:    tailscale_id.Name,
+			Healthy: true,
+		},
+	}, nil
 }
 
 func (c *component) Events(ctx context.Context, since time.Time) ([]components.Event, error) {
@@ -90,9 +47,6 @@ func (c *component) Metrics(ctx context.Context, since time.Time) ([]components.
 
 func (c *component) Close() error {
 	log.Logger.Debugw("closing component")
-
-	// safe to call stop multiple times
-	c.poller.Stop(tailscale_id.Name)
 
 	return nil
 }
