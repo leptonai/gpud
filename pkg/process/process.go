9d994b6e8402a67934e8968355504c31ae8e135b
Gyuho Lee <gyuhox@gmail.com>
2025-01-21 21:56:57 +0800

    lower log level
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- pkg/process/process.go
+++ pkg/process/process.go
@@ -313,7 +313,7 @@ func (p *process) watchCmd() {
 						log.Logger.Warnw("command was terminated (exit code -1) for unknown reasons", "cmd", p.cmd.String())
 					}
 				} else {
-					log.Logger.Warnw("command exited with non-zero status", "error", err, "cmd", p.cmd.String(), "exitCode", exitErr.ExitCode())
+					log.Logger.Debugw("command exited with non-zero status", "error", err, "cmd", p.cmd.String(), "exitCode", exitErr.ExitCode())
 				}
 			} else {
 				log.Logger.Warnw("error waiting for command to finish", "error", err, "cmd", p.cmd.String())
