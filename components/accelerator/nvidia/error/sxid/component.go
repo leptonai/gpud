7e9003bd94ea975a37348cea712f3cab0aafbeda
Cardy Tang <zuniorone@gmail.com>
2025-02-03 23:07:08 +0800

    feat(sxid): simplify sxid component  (#337)
    Signed-off-by: cardyok <zuniorone@gmail.com>

--- components/accelerator/nvidia/error/sxid/component.go
+++ components/accelerator/nvidia/error/sxid/component.go
@@ -4,95 +4,235 @@ package sxid
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
 	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/leptonai/gpud/components"
 	nvidia_component_error_sxid_id "github.com/leptonai/gpud/components/accelerator/nvidia/error/sxid/id"
-	nvidia_query_sxid "github.com/leptonai/gpud/components/accelerator/nvidia/query/sxid"
-	"github.com/leptonai/gpud/components/dmesg"
+	"github.com/leptonai/gpud/components/accelerator/nvidia/query/sxid/dmesg"
+	"github.com/leptonai/gpud/components/db"
+	os_id "github.com/leptonai/gpud/components/os/id"
 	"github.com/leptonai/gpud/log"
+	pkg_dmesg "github.com/leptonai/gpud/pkg/dmesg"
 )
 
-func New() components.Component {
-	return &component{}
+const (
+	StateNameErrorSXid = "error_sxid"
+
+	EventNameErroSXid    = "error_sxid"
+	EventKeyErroSXidData = "data"
+	EventKeyDeviceUUID   = "device_uuid"
+
+	DefaultRetentionPeriod   = 3 * 24 * time.Hour
+	DefaultStateUpdatePeriod = 30 * time.Second
+)
+
+type SXIDComponent struct {
+	rootCtx      context.Context
+	cancel       context.CancelFunc
+	currState    components.State
+	extraEventCh chan *components.Event
+	store        db.Store
+	mu           sync.RWMutex
 }
 
-var _ components.Component = (*component)(nil)
+func New(ctx context.Context, dbRW *sql.DB, dbRO *sql.DB) *SXIDComponent {
+	cctx, ccancel := context.WithCancel(ctx)
 
-type component struct{}
-
-func (c *component) Name() string { return nvidia_component_error_sxid_id.Name }
-
-func (c *component) Start() error { return nil }
-
-func (c *component) States(ctx context.Context) ([]components.State, error) {
-	return []components.State{{
-		Name:    StateNameErrorSXid,
-		Healthy: true,
-		Reason:  "sxid monitoring working",
-	}}, nil
+	extraEventCh := make(chan *components.Event, 256)
+	localStore, err := db.NewStore(dbRW, dbRO, db.CreateDefaultTableName(nvidia_component_error_sxid_id.Name), DefaultRetentionPeriod)
+	if err != nil {
+		log.Logger.Errorw("failed to create store", "error", err)
+		ccancel()
+		return nil
+	}
+	return &SXIDComponent{
+		rootCtx:      cctx,
+		cancel:       ccancel,
+		extraEventCh: extraEventCh,
+		store:        localStore,
+	}
 }
 
-// tailScan fetches the latest output from the dmesg
-// it is ok to call this function multiple times for the following reasons (thus shared with events method)
-// 1) dmesg "TailScan" is cheap (just tails the last x number of lines)
-func (c *component) tailScan() (*Output, error) {
-	dmesgC, err := components.GetComponent(dmesg.Name)
+var _ components.Component = (*SXIDComponent)(nil)
+
+func (c *SXIDComponent) Name() string { return nvidia_component_error_sxid_id.Name }
+
+func (c *SXIDComponent) Start() error {
+	initializeBackoff := 1 * time.Second
+	for {
+		if err := c.updateCurrentState(); err != nil {
+			if strings.Contains(err.Error(), context.Canceled.Error()) {
+				log.Logger.Infow("context canceled, exiting")
+				return nil
+			}
+			log.Logger.Errorw("failed to fetch current events", "error", err)
+			time.Sleep(initializeBackoff)
+			continue
+		}
+		break
+	}
+	watcher, err := pkg_dmesg.NewWatcher()
+	if err != nil {
+		log.Logger.Errorw("failed to create dmesg watcher", "error", err)
+		return nil
+	}
+
+	go c.start(watcher, DefaultStateUpdatePeriod)
+
+	return nil
+}
+
+func (c *SXIDComponent) States(ctx context.Context) ([]components.State, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return []components.State{c.currState}, nil
+}
+
+func (c *SXIDComponent) Events(ctx context.Context, since time.Time) ([]components.Event, error) {
+	var ret []components.Event
+	events, err := c.store.Get(ctx, since)
 	if err != nil {
 		return nil, err
 	}
-
-	var dmesgComponent *dmesg.Component
-	if o, ok := dmesgC.(interface{ Unwrap() interface{} }); ok {
-		if unwrapped, ok := o.Unwrap().(*dmesg.Component); ok {
-			dmesgComponent = unwrapped
-		} else {
-			return nil, fmt.Errorf("expected *dmesg.Component, got %T", dmesgC)
-		}
+	for _, event := range events {
+		ret = append(ret, resolveSXIDEvent(event))
 	}
-	dmesgTailResults, err := dmesgComponent.TailScan()
-	if err != nil {
-		return nil, err
-	}
-
-	o := &Output{}
-	for _, logItem := range dmesgTailResults.TailScanMatched {
-		if logItem.Error != nil {
-			continue
-		}
-		if logItem.Matched == nil {
-			continue
-		}
-		if logItem.Matched.Name != dmesg.EventNvidiaNVSwitchSXid {
-			continue
-		}
-
-		ev, err := nvidia_query_sxid.ParseDmesgLogLine(logItem.Time, logItem.Line)
-		if err != nil {
-			return nil, err
-		}
-		o.DmesgErrors = append(o.DmesgErrors, ev)
-	}
-
-	return o, nil
+	return ret, nil
 }
 
-func (c *component) Events(ctx context.Context, since time.Time) ([]components.Event, error) {
-	o, err := c.tailScan()
-	if err != nil {
-		return nil, err
-	}
-	return o.getEvents(since), nil
-}
-
-func (c *component) Metrics(ctx context.Context, since time.Time) ([]components.Metric, error) {
+func (c *SXIDComponent) Metrics(ctx context.Context, since time.Time) ([]components.Metric, error) {
 	log.Logger.Debugw("querying metrics", "since", since)
 
 	return nil, nil
 }
 
-func (c *component) Close() error {
-	log.Logger.Debugw("closing component")
+func (c *SXIDComponent) Close() error {
+	log.Logger.Debugw("closing SXIDComponent")
+	c.cancel()
 	return nil
+}
+
+func (c *SXIDComponent) start(watcher pkg_dmesg.Watcher, updatePeriod time.Duration) {
+	ticker := time.NewTicker(updatePeriod)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-c.rootCtx.Done():
+			return
+		case <-ticker.C:
+			if err := c.updateCurrentState(); err != nil {
+				log.Logger.Debugw("failed to fetch current events", "error", err)
+				continue
+			}
+		case newEvent := <-c.extraEventCh:
+			if newEvent == nil {
+				continue
+			}
+			if err := c.store.Insert(c.rootCtx, *newEvent); err != nil {
+				log.Logger.Errorw("failed to create event", "error", err)
+				continue
+			}
+			events, err := c.store.Get(c.rootCtx, time.Time{})
+			if err != nil {
+				log.Logger.Errorw("failed to get all events", "error", err)
+				continue
+			}
+			c.mu.Lock()
+			c.currState = EvolveHealthyState(events)
+			c.mu.Unlock()
+		case dmesgLine := <-watcher.Watch():
+			log.Logger.Debugw("dmesg line", "line", dmesgLine)
+			sxidErr := dmesg.Match(dmesgLine.Content)
+			if sxidErr == nil {
+				log.Logger.Debugw("not xid event, skip")
+				continue
+			}
+			event := components.Event{
+				Time: metav1.Time{Time: dmesgLine.Timestamp},
+				Name: EventNameErroSXid,
+				ExtraInfo: map[string]string{
+					EventKeyErroSXidData: strconv.FormatInt(int64(sxidErr.SXid), 10),
+					EventKeyDeviceUUID:   sxidErr.DeviceUUID,
+				},
+			}
+			currEvent, err := c.store.Find(c.rootCtx, event)
+			if err != nil {
+				log.Logger.Errorw("failed to check event existence", "error", err)
+				continue
+			}
+
+			if currEvent != nil {
+				log.Logger.Debugw("no new events created")
+				continue
+			}
+			if err = c.store.Insert(c.rootCtx, event); err != nil {
+				log.Logger.Errorw("failed to create event", "error", err)
+				continue
+			}
+			events, err := c.store.Get(c.rootCtx, time.Time{})
+			if err != nil {
+				log.Logger.Errorw("failed to get all events", "error", err)
+				continue
+			}
+			c.mu.Lock()
+			c.currState = EvolveHealthyState(events)
+			c.mu.Unlock()
+		}
+	}
+}
+
+func (c *SXIDComponent) SetHealthy() error {
+	log.Logger.Debugw("set healthy event received")
+	newEvent := &components.Event{Time: metav1.Time{Time: time.Now().UTC()}, Name: "SetHealthy"}
+	select {
+	case c.extraEventCh <- newEvent:
+	default:
+		log.Logger.Debugw("channel full, set healthy event skipped")
+	}
+	return nil
+}
+
+func (c *SXIDComponent) updateCurrentState() error {
+	osComponent, err := components.GetComponent(os_id.Name)
+	if err != nil {
+		return fmt.Errorf("failed to get os component: %w", err)
+	}
+	osEvents, err := osComponent.Events(c.rootCtx, time.Now().Add(-DefaultRetentionPeriod))
+	if err != nil {
+		return fmt.Errorf("failed to get os events: %w", err)
+	}
+	localEvents, err := c.store.Get(c.rootCtx, time.Time{})
+	if err != nil {
+		return fmt.Errorf("failed to get all events: %w", err)
+	}
+	events := mergeEvents(osEvents, localEvents)
+	c.mu.Lock()
+	c.currState = EvolveHealthyState(events)
+	c.mu.Unlock()
+	return nil
+}
+
+// mergeEvents merges two event slices and returns a time descending sorted new slice
+func mergeEvents(a, b []components.Event) []components.Event {
+	totalLen := len(a) + len(b)
+	if totalLen == 0 {
+		return nil
+	}
+	result := make([]components.Event, 0, totalLen)
+	result = append(result, a...)
+	result = append(result, b...)
+
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].Time.Time.After(result[j].Time.Time)
+	})
+
+	return result
 }
---
+++ components/accelerator/nvidia/error/sxid/helper.go
@@ -0,0 +1,136 @@
+package sxid
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"github.com/leptonai/gpud/components"
+	nvidia_query_sxid "github.com/leptonai/gpud/components/accelerator/nvidia/query/sxid"
+	"github.com/leptonai/gpud/components/common"
+	"github.com/leptonai/gpud/log"
+)
+
+const (
+	StateHealthy   = 0
+	StateDegraded  = 1
+	StateUnhealthy = 2
+)
+
+const rebootThreshold = 2
+
+// EvolveHealthyState resolves the state of the SXID error component.
+// note: assume events are sorted by time in descending order
+func EvolveHealthyState(events []components.Event) (ret components.State) {
+	defer func() {
+		log.Logger.Debugf("EvolveHealthyState: %v", ret)
+	}()
+	var lastSuggestedAction *common.SuggestedActions
+	var lastSXidErr *SXidError
+	lastHealth := StateHealthy
+	sxidRebootMap := make(map[uint64]int)
+	for i := len(events) - 1; i >= 0; i-- {
+		event := events[i]
+		log.Logger.Debugf("EvolveHealthyState: event: %v %v %+v %+v %+v", event.Time, event.Name, lastSuggestedAction, sxidRebootMap, lastSXidErr)
+		if event.Name == EventNameErroSXid {
+			resolvedEvent := resolveSXIDEvent(event)
+			var currSXidErr SXidError
+			if err := json.Unmarshal([]byte(resolvedEvent.ExtraInfo[EventKeyErroSXidData]), &currSXidErr); err != nil {
+				log.Logger.Errorf("failed to unmarshal event %s %s extra info: %s", resolvedEvent.Name, resolvedEvent.Message, err)
+				continue
+			}
+
+			currEvent := StateHealthy
+			switch resolvedEvent.Type {
+			case common.EventTypeCritical:
+				currEvent = StateDegraded
+			case common.EventTypeFatal:
+				currEvent = StateUnhealthy
+			}
+			if currEvent < lastHealth {
+				continue
+			}
+			lastHealth = currEvent
+			lastSXidErr = &currSXidErr
+			if currSXidErr.SuggestedActionsByGPUd != nil && len(currSXidErr.SuggestedActionsByGPUd.RepairActions) > 0 {
+				if currSXidErr.SuggestedActionsByGPUd.RepairActions[0] == common.RepairActionTypeRebootSystem {
+					if count, ok := sxidRebootMap[currSXidErr.SXid]; !ok {
+						sxidRebootMap[currSXidErr.SXid] = 0
+					} else if count >= rebootThreshold {
+						currSXidErr.SuggestedActionsByGPUd.RepairActions[0] = common.RepairActionTypeHardwareInspection
+					}
+				}
+				lastSuggestedAction = currSXidErr.SuggestedActionsByGPUd
+			}
+		} else if event.Name == "reboot" {
+			if lastSuggestedAction != nil && len(lastSuggestedAction.RepairActions) > 0 && (lastSuggestedAction.RepairActions[0] == common.RepairActionTypeRebootSystem || lastSuggestedAction.RepairActions[0] == common.RepairActionTypeCheckUserAppAndGPU) {
+				lastHealth = StateHealthy
+				lastSuggestedAction = nil
+				lastSXidErr = nil
+			}
+			for sxid, count := range sxidRebootMap {
+				sxidRebootMap[sxid] = count + 1
+			}
+		} else if event.Name == "SetHealthy" {
+			lastHealth = StateHealthy
+			lastSuggestedAction = nil
+			lastSXidErr = nil
+			sxidRebootMap = make(map[uint64]int)
+		}
+	}
+	var reason string
+	var stateError string
+	if lastSXidErr == nil {
+		reason = "SXIDComponent is healthy"
+	} else {
+		xidErrBytes, _ := lastSXidErr.JSON()
+		reason = string(xidErrBytes)
+		stateError = fmt.Sprintf("sxid %d detected by %s", lastSXidErr.SXid, lastSXidErr.DataSource)
+	}
+	return components.State{
+		Name:             StateNameErrorSXid,
+		Healthy:          lastHealth == StateHealthy,
+		Health:           translateToStateHealth(lastHealth),
+		Reason:           reason,
+		Error:            stateError,
+		SuggestedActions: lastSuggestedAction,
+	}
+}
+
+func translateToStateHealth(health int) string {
+	switch health {
+	case StateHealthy:
+		return components.StateHealthy
+	case StateDegraded:
+		return components.StateDegraded
+	case StateUnhealthy:
+		return components.StateUnhealthy
+	default:
+		return components.StateHealthy
+	}
+}
+
+func resolveSXIDEvent(event components.Event) components.Event {
+	ret := event
+	if event.ExtraInfo != nil {
+		if currSXid, err := strconv.Atoi(event.ExtraInfo[EventKeyErroSXidData]); err == nil {
+			detail, ok := nvidia_query_sxid.GetDetail(currSXid)
+			if !ok {
+				return ret
+			}
+			ret.Type = detail.EventType
+			ret.Message = fmt.Sprintf("XID %d detected on %s", currSXid, event.ExtraInfo[EventKeyDeviceUUID])
+			ret.SuggestedActions = detail.SuggestedActionsByGPUd
+			raw, _ := json.Marshal(&SXidError{
+				Time:                      event.Time,
+				DataSource:                "dmesg",
+				DeviceUUID:                event.ExtraInfo[EventKeyDeviceUUID],
+				SXid:                      uint64(currSXid),
+				SuggestedActionsByGPUd:    detail.SuggestedActionsByGPUd,
+				CriticalErrorMarkedByGPUd: detail.CriticalErrorMarkedByGPUd,
+			})
+			ret.ExtraInfo[EventKeyErroSXidData] = string(raw)
+		}
+	}
+	return ret
+}
