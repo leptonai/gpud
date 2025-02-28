dea38fb9e5ff9545daa7a3df964a903fabcb18bb
Gyuho Lee <6799218+gyuho@users.noreply.github.com>
2025-02-28 16:08:50 +0800

    feta(pkg/update): split into smaller files, add unit tests (#473)
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>
    
    ---------
    
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- pkg/update/update_test.go
+++ pkg/update/update_test.go
@@ -1,19 +1,1 @@
 package update
-
-import (
-	"fmt"
-	"testing"
-)
-
-func Test_tarballName(t *testing.T) {
-	ubuntuVer := detectUbuntuVersion()
-	ubuntuSuffix := ""
-	if ubuntuVer != "" {
-		ubuntuSuffix = "_" + ubuntuVer
-	}
-	name := tarballName("v0.0.1", "linux", "amd64")
-	want := fmt.Sprintf("gpud_v0.0.1_linux_amd64%s.tgz", ubuntuSuffix)
-	if name != want {
-		t.Fatalf("want: %s, got: %s", want, name)
-	}
-}
