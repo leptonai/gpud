c4b4f9c4301c909234298be86293d2524ea5317b
Gyuho Lee <gyuhox@gmail.com>
2025-04-30 11:01:29 +0800

    feat(login, gossip): log at debug level
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- cmd/gpud/command/login.go
+++ cmd/gpud/command/login.go
@@ -106,7 +106,7 @@ func cmdLogin(cliContext *cli.Context) error {
 	// and then "gpud up"
 	// we still need this in case "gpud up" and then "gpud login" afterwards
 	if err := server.WriteToken(token, fifoFile); err != nil {
-		log.Logger.Warnw("failed to write token -- login before first gpud run/up", "error", err)
+		log.Logger.Debugw("failed to write token -- login before first gpud run/up", "error", err)
 	}
 
 	if err = gpudstate.UpdateLoginInfo(rootCtx, dbRW, machineID, token); err != nil {
