df7d76480e529a4b15ef0565b516c784fe7e3c95
Tao Li <lita@nvidia.com>
2025-06-22 16:57:00 +0800

    add UT
    Signed-off-by: Tao Li <lita@nvidia.com>

--- pkg/providers/nebius/nebius.go
+++ pkg/providers/nebius/nebius.go
@@ -3,25 +3,30 @@ package nebius
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+)
+
+var (
+	metadataPath = "/mnt/cloud-metadata"
 )
 
 func GetInstanceID() (string, error) {
-	projectID, err := os.ReadFile("/mnt/cloud-metadata/parent-id")
+	projectID, err := os.ReadFile(filepath.Join(metadataPath, "parent-id"))
 	if err != nil {
 		return "", err
 	}
-	gpuClusterID, err := os.ReadFile("/mnt/cloud-metadata/gpu-cluster-id")
+	gpuClusterID, err := os.ReadFile(filepath.Join(metadataPath, "gpu-cluster-id"))
 	if err != nil {
 		if !os.IsNotExist(err) {
 			return "", err
 		}
 	}
-	instanceID, err := os.ReadFile("/mnt/cloud-metadata/instance-id")
+	instanceID, err := os.ReadFile(filepath.Join(metadataPath, "instance-id"))
 	if err != nil {
 		return "", err
 	}
 	if len(gpuClusterID) > 0 {
-		return fmt.Sprintf("projects/%s/gpu-clusters/%s/instances/%s", string(projectID), string(gpuClusterID), string(instanceID)), nil
+		return fmt.Sprintf("%s/%s/%s", string(projectID), string(gpuClusterID), string(instanceID)), nil
 	}
-	return fmt.Sprintf("projects/%s/instances/%s", string(projectID), string(instanceID)), nil
+	return fmt.Sprintf("%s/%s", string(projectID), string(instanceID)), nil
 }
