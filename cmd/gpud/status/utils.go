6d15f9bacc57d4c277533446793a5d8e08cb69e4
Gyuho Lee <gyuhol@nvidia.com>
2025-06-30 22:24:30 +0800

    do nothing if empty
    Signed-off-by: Gyuho Lee <gyuhol@nvidia.com>

--- cmd/gpud/status/utils.go
+++ cmd/gpud/status/utils.go
@@ -11,17 +11,18 @@ import (
 )
 
 func checkLoginSuccess(loginSuccess, machineID string) error {
-	if loginSuccess != "" {
-		ts, err := strconv.ParseInt(loginSuccess, 10, 64)
-		if err != nil {
-			return fmt.Errorf("failed to parse login success: %w", err)
-		}
-		loginTimeUTC := time.Unix(ts, 0)
-		nowUTC := time.Now().UTC()
-		loginTimeHumanized := humanize.RelTime(loginTimeUTC, nowUTC, "ago", "from now")
-		fmt.Printf("%s login success at %s (machine id: %s)\n", cmdcommon.CheckMark, loginTimeHumanized, machineID)
-	} else {
-		fmt.Printf("%s login information not found\n", cmdcommon.CheckMark)
+	if loginSuccess == "" {
+		return nil
 	}
+
+	ts, err := strconv.ParseInt(loginSuccess, 10, 64)
+	if err != nil {
+		return fmt.Errorf("failed to parse login success: %w", err)
+	}
+	loginTimeUTC := time.Unix(ts, 0)
+	nowUTC := time.Now().UTC()
+	loginTimeHumanized := humanize.RelTime(loginTimeUTC, nowUTC, "ago", "from now")
+	fmt.Printf("%s login success at %s (machine id: %s)\n", cmdcommon.CheckMark, loginTimeHumanized, machineID)
+
 	return nil
 }
