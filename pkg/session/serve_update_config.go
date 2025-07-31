07cb1134f390457f4acf4e3e344fae52e5d101b7
Gyuho Lee <6799218+gyuho@users.noreply.github.com>
2025-07-31 17:26:05 +0800

    [LEP-1395] fix(session): fallback to default empty config if the component is not set (#1022)
    Signed-off-by: Gyuho Lee <gyuhol@nvidia.com>

--- pkg/session/serve_update_config.go
+++ pkg/session/serve_update_config.go
@@ -18,11 +18,13 @@ func (s *Session) processUpdateConfig(configMap map[string]string, resp *Respons
 		return
 	}
 
+	setComponents := make(map[string]any)
 	for componentName, value := range configMap {
 		log.Logger.Infow("processing update config request", "component", componentName, "config", value)
 
 		switch componentName {
 		case componentsnvidiainfiniband.Name:
+			setComponents[componentName] = struct{}{}
 			var updateCfg infiniband.ExpectedPortStates
 			if err := json.Unmarshal([]byte(value), &updateCfg); err != nil {
 				log.Logger.Warnw("failed to unmarshal infiniband config", "error", err)
@@ -34,6 +36,7 @@ func (s *Session) processUpdateConfig(configMap map[string]string, resp *Respons
 			}
 
 		case componentsnvidiagpucounts.Name:
+			setComponents[componentName] = struct{}{}
 			var updateCfg componentsnvidiagpucounts.ExpectedGPUCounts
 			if err := json.Unmarshal([]byte(value), &updateCfg); err != nil {
 				log.Logger.Warnw("failed to unmarshal nvidia gpu counts config", "error", err)
@@ -45,6 +48,7 @@ func (s *Session) processUpdateConfig(configMap map[string]string, resp *Respons
 			}
 
 		case componentsnfs.Name:
+			setComponents[componentName] = struct{}{}
 			var updateCfgs pkgnfschecker.Configs
 			if err := json.Unmarshal([]byte(value), &updateCfgs); err != nil {
 				log.Logger.Warnw("failed to unmarshal nfs config", "error", err)
@@ -67,5 +71,19 @@ func (s *Session) processUpdateConfig(configMap map[string]string, resp *Respons
 		default:
 			log.Logger.Warnw("unsupported component for updateConfig", "component", componentName)
 		}
+	}
+
+	// fallback to default empty if the component is not set
+	if _, ok := setComponents[componentsnvidiainfiniband.Name]; !ok && s.setDefaultIbExpectedPortStatesFunc != nil {
+		log.Logger.Infow("falling back to default empty infiniband config")
+		s.setDefaultIbExpectedPortStatesFunc(infiniband.ExpectedPortStates{})
+	}
+	if _, ok := setComponents[componentsnvidiagpucounts.Name]; !ok && s.setDefaultGPUCountsFunc != nil {
+		log.Logger.Infow("falling back to default empty nvidia gpu counts config")
+		s.setDefaultGPUCountsFunc(componentsnvidiagpucounts.ExpectedGPUCounts{})
+	}
+	if _, ok := setComponents[componentsnfs.Name]; !ok && s.setDefaultNFSGroupConfigsFunc != nil {
+		log.Logger.Infow("falling back to default empty nfs config")
+		s.setDefaultNFSGroupConfigsFunc(pkgnfschecker.Configs{})
 	}
 }
