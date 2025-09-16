fb9d0e8b8a383fb30f87b58c492418296bc67327
Gyuho Lee <gyuhol@nvidia.com>
2025-09-16 17:49:11 +0800

    feat(xid,sxid,gpu-counts): enable event store purge after 14 days
    Signed-off-by: Gyuho Lee <gyuhol@nvidia.com>

--- components/accelerator/nvidia/xid/component.go
+++ components/accelerator/nvidia/xid/component.go
@@ -89,7 +89,7 @@ func New(gpudInstance *components.GPUdInstance) (components.Component, error) {
 
 	if gpudInstance.EventStore != nil {
 		var err error
-		c.eventBucket, err = gpudInstance.EventStore.Bucket(Name, eventstore.WithDisablePurge())
+		c.eventBucket, err = gpudInstance.EventStore.Bucket(Name)
 		if err != nil {
 			ccancel()
 			return nil, err
