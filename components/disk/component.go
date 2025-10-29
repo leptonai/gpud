8ca519b34e8f273e12b436ae06f84215593b95d3
Gyuho Lee <6799218+gyuho@users.noreply.github.com>
2025-10-28 22:23:07 -0700

    feat(components/disk): remove unnecessary context timeout setups, improve logging around timeouts (#1119)
    Signed-off-by: Gyuho Lee <gyuhol@nvidia.com>
    
    ---------
    
    Signed-off-by: Gyuho Lee <gyuhol@nvidia.com>

--- components/disk/component.go
+++ components/disk/component.go
@@ -81,7 +81,8 @@ const (
 
 	defaultFreeSpaceThresholdBytesDegraded = 500 * 1024 * 1024 // 500 MB
 
-	getPartitionsTimeout = 10 * time.Second
+	getBlockDevicesTimeout = 10 * time.Second
+	getPartitionsTimeout   = 10 * time.Second
 )
 
 func New(gpudInstance *components.GPUdInstance) (components.Component, error) {
@@ -125,8 +126,10 @@ func New(gpudInstance *components.GPUdInstance) (components.Component, error) {
 	if runtime.GOOS == "linux" {
 		// relies on "lsblk" command
 		c.getBlockDevicesFunc = func(ctx context.Context) (disk.BlockDevices, error) {
+			timeoutCtx, cancel := context.WithTimeout(ctx, getBlockDevicesTimeout)
+			defer cancel()
 			return disk.GetBlockDevicesWithLsblk(
-				ctx,
+				timeoutCtx,
 				disk.WithFstype(disk.DefaultFsTypeFunc),
 				disk.WithDeviceType(disk.DefaultDeviceTypeFunc),
 				disk.WithMountPoint(disk.DefaultMountPointFunc),
@@ -665,18 +668,27 @@ func (c *component) fetchBlockDevices(cr *checkResult) bool {
 	// "unexpected end of JSON input"
 	prevFailed := false
 	for i := 0; i < 5; i++ {
-		cctx, ccancel := context.WithTimeout(c.ctx, time.Minute)
-		blks, err := c.getBlockDevicesFunc(cctx)
-		ccancel()
+		blks, err := c.getBlockDevicesFunc(c.ctx) // "getBlockDevicesFunc" itself already sets its own timeout
 		if err != nil {
-			log.Logger.Errorw("failed to get block devices", "error", err)
 
 			select {
 			case <-c.ctx.Done():
 				cr.health = apiv1.HealthStateTypeUnhealthy
 				cr.err = c.ctx.Err()
+
+				if cr.reason == "ok" {
+					cr.reason = ""
+				}
+				if cr.reason != "" {
+					cr.reason += "; "
+				}
+				cr.reason += "failed to get block devices -- took too long"
+
+				log.Logger.Warnw("failed to get block devices -- took too long", "error", err)
 				return false
+
 			case <-time.After(c.retryInterval):
+				log.Logger.Warnw("failed to get block devices -- retrying", "error", err)
 			}
 
 			prevFailed = true
@@ -715,15 +727,12 @@ func (c *component) fetchExt4Partitions(cr *checkResult) bool {
 	// "unexpected end of JSON input"
 	prevFailed := false
 	for i := 0; i < 5; i++ {
-		cctx, ccancel := context.WithTimeout(c.ctx, time.Minute)
-		parts, err := c.getExt4PartitionsFunc(cctx)
-		ccancel()
+		parts, err := c.getExt4PartitionsFunc(c.ctx) // "getExt4PartitionsFunc" itself already sets its own timeout
 		if err != nil {
-			log.Logger.Errorw("failed to get ext4 partitions", "error", err)
-
 			select {
 			case <-c.ctx.Done():
 				cr.health = apiv1.HealthStateTypeUnhealthy
+				cr.err = c.ctx.Err()
 
 				if cr.reason == "ok" {
 					cr.reason = ""
@@ -731,11 +740,13 @@ func (c *component) fetchExt4Partitions(cr *checkResult) bool {
 				if cr.reason != "" {
 					cr.reason += "; "
 				}
-				cr.reason += "failed to get ext4 partitions"
+				cr.reason += "failed to get ext4 partitions -- took too long"
 
-				cr.err = c.ctx.Err()
+				log.Logger.Warnw("failed to get ext4 partitions -- took too long", "error", err)
 				return false
+
 			case <-time.After(c.retryInterval):
+				log.Logger.Warnw("failed to get ext4 partitions -- retrying", "error", err)
 			}
 
 			prevFailed = true
@@ -754,18 +765,26 @@ func (c *component) fetchExt4Partitions(cr *checkResult) bool {
 func (c *component) fetchNFSPartitions(cr *checkResult) bool {
 	prevFailed := false
 	for i := 0; i < 5; i++ {
-		cctx, ccancel := context.WithTimeout(c.ctx, time.Minute)
-		parts, err := c.getNFSPartitionsFunc(cctx)
-		ccancel()
+		parts, err := c.getNFSPartitionsFunc(c.ctx) // "getNFSPartitionsFunc" itself already sets its own timeout
 		if err != nil {
-			log.Logger.Errorw("failed to get nfs partitions", "error", err)
-
 			select {
 			case <-c.ctx.Done():
 				cr.health = apiv1.HealthStateTypeUnhealthy
 				cr.err = c.ctx.Err()
+
+				if cr.reason == "ok" {
+					cr.reason = ""
+				}
+				if cr.reason != "" {
+					cr.reason += "; "
+				}
+				cr.reason += "failed to get nfs partitions -- took too long"
+
+				log.Logger.Warnw("failed to get nfs partitions -- took too long", "error", err)
 				return false
+
 			case <-time.After(c.retryInterval):
+				log.Logger.Warnw("failed to get nfs partitions -- retrying", "error", err)
 			}
 
 			prevFailed = true
