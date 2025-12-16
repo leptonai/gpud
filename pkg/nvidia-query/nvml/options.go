9c34f4b1a27598eba4b8ae65fbc42da15fafe2d3
Gyuho Lee <gyuhol@nvidia.com>
2025-12-16 14:09:35 +0800

    remove unused op option
    Signed-off-by: Gyuho Lee <gyuhol@nvidia.com>

--- pkg/nvidia-query/nvml/options.go
+++ pkg/nvidia-query/nvml/options.go
@@ -1,55 +1,18 @@
 package nvml
 
 import (
-	"database/sql"
-
 	"github.com/leptonai/gpud/pkg/eventstore"
-	"github.com/leptonai/gpud/pkg/sqlite"
 )
 
 type Op struct {
-	dbRW                  *sql.DB
-	dbRO                  *sql.DB
 	hwSlowdownEventBucket eventstore.Bucket
 }
 
 type OpOption func(*Op)
 
-func (op *Op) applyOpts(opts []OpOption) error {
+func (op *Op) applyOpts(opts []OpOption) {
 	for _, opt := range opts {
 		opt(op)
-	}
-	if op.dbRW == nil {
-		var err error
-		op.dbRW, err = sqlite.Open(":memory:")
-		if err != nil {
-			return err
-		}
-	}
-	if op.dbRO == nil {
-		var err error
-		op.dbRO, err = sqlite.Open(":memory:", sqlite.WithReadOnly(true))
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// Specifies the database instance to persist nvidia components data
-// (e.g., xid/sxid events). Must be a writable database instance.
-// If not specified, a new in-memory database is created.
-func WithDBRW(db *sql.DB) OpOption {
-	return func(op *Op) {
-		op.dbRW = db
-	}
-}
-
-// Specifies the read-only database instance.
-// If not specified, a new in-memory database is created.
-func WithDBRO(db *sql.DB) OpOption {
-	return func(op *Op) {
-		op.dbRO = db
 	}
 }
 
